e4862a296a0c606d77695a2c689ec514280f6247
William Bojczuk <williambojczuk@gmail.com>
2023-08-20 14:13:03 -0700

    Added services

--- src/components/Services/serviceData.ts
+++ src/components/Services/serviceData.ts
@@ -7,63 +7,83 @@ interface Service{
 
 const serviceData: Service[] = [
     
-    
     {
         img: "./img/mainbg.jpeg",
-        name: "Example Service",
-        shortDesc: "A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.",
-        desc: `A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
+        name: "House / building washing",
+        shortDesc: "Every house or building should be cleaned one to two times a year from mold, mildew and algae. These organic growths will cause serious damages if left untreated. ",
+        desc: `Every house or building should be cleaned one to two times a year from mold, mildew and algae. These organic growths will cause serious damages if left untreated. 
         
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
+        We use a low pressure system to safely clean your house or building. High pressure will cause serious damages and a potential for injury risks. Our system is able to apply cleaning solution three to four stories high, at the pressure of a residential water spigot. 
+
+        Our solution is safe for all types of siding including vinyl, hardie board, stucco, brick, wood, aluminum, and concrete. We also water all plant life around the house or building being washed for added protection. `
+    },
+
+
+    {
+        img: "./img/mainbg.jpeg",
+        name: "Roof washing",
+        shortDesc: "Asphalt shingle roofs need to be washed because a type of algae know as “Gloeocapsa magma” develops on the roofing system. ",
+        desc: `Asphalt shingle roofs need to be washed because a type of algae know as “Gloeocapsa magma” develops on the roofing system. This type of algae feeds on limestone filler found in asphalt shingles. Many other organisms can develop as a result of this algae bacteria, such as moss, lichen, mold, and mildew. These organic growths will cause damages to your roof, depreciate value, decrease curb appeal, and shorten the life of your roof. 
+
+        We use a low pressure system to safely apply cleaning solution to the roofing system. High pressure on any roof will cause serious damages including, extreme granule loss, roofing leaks, cracked tile, and blown off shingles. 
         
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.`
+        Our solution is 100% guaranteed to remove these organics from your roofing system. This will remove dark stains, increasing value, extending roof life, and boosting curb appeal. `
+    },
+
+    {
+        img: "./img/mainbg.jpeg",
+        name: "Concrete Washing",
+        shortDesc: "Mold, mildew, and algae develops on concrete surfaces. This makes concrete have a dark appearance, and makes the surface slick when wet.",
+        desc: `Mold, mildew, and algae develops on concrete surfaces. This makes concrete have a dark appearance, and makes the surface slick when wet. Concrete is also a porous surface which absorbs liquid matter, and leaves stains in it. Grease, oil, rust, red clay are main examples of stains found in concrete. 
+
+        Our cleaning solution is applied to the concrete surface to kill all organic bacteria, and remove stains. 
+        
+        Concrete must be properly cleaned or permanent etching or damages will happen. Only a certain P.S.I is to be used when cleaning concrete. We use a commercial surface cleaner to evenly clean the surface leaving no lines, streaks, or etching.`
+    },
+
+
+    {
+        img: "./img/mainbg.jpeg",
+        name: "Deck / fence cleaning",
+        shortDesc: "Over time wood becomes weathered, warped, sagging, and unsafe. Wood must be properly sealed with stain, paint, or sealer to prevent damages from the elements. ",
+        desc: `Wooden surfaces must be properly cleaned and maintained to sustain the life of your investment. Over time wood becomes weathered, warped, sagging, and unsafe. Wood must be properly sealed with stain, paint, or sealer to prevent damages from the elements. 
+
+        In our deck / fence cleaning service, we treat the wood for all organic bacteria that eats away the wood. We use a medium pressure to remove the weathered appearance of the wood. This will properly clean the wood surface, extending the life of your investment, and boosting value and appeal!
+        
+        Painted wood is always soft washed to protect the paint on the surface. Extra precautions must be taken when a surface has been sealed or stained, as some solutions will strip the wood of the sealer. `
+    },
+
+    {
+        img: "./img/mainbg.jpeg",
+        name: "Gutter brightening",
+        shortDesc: "Overtime gutters develop black stains or streaks, making the appearance dark and dull. This happens from rainwater running down an asphalt shingle roof. The asphalt soaks into the metal leaving these dark stains or streaks.",
+        desc: `Overtime gutters develop black stains or streaks, making the appearance dark and dull. This happens from rainwater running down an asphalt shingle roof. The asphalt soaks into the metal leaving these dark stains or streaks. 
+
+        We apply a cleaning solution designed to remove this stain, making your gutters nice and bright! This is a job for a trained professional, because paint can actually be removed from the gutter itself if done incorrectly, or with the incorrect solutions.        
+        
+        This will boost curb appeal, add value to your property. Gutter brightening stain removal id an excellent add on to your house or building wash!`
     },
     {
         img: "./img/mainbg.jpeg",
-        name: "Example Service",
-        shortDesc: "A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.",
-        desc: `A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
+        name: "Gutter cleaning",
+        shortDesc: "Gutters should be cleaned at least once a year. Leaves, pine straw, and dirt collects in the gutter, restricting water flow, creating a habitat for insects, and creates extra weight on your fascia board. ",
+        desc: `Gutters should be cleaned at least once a year. Leaves, pine straw, and dirt collects in the gutter, restricting water flow, creating a habitat for insects, and creates extra weight on your fascia board. 
+
+        Gutters channel the water runoff from a roofing system. This is designed to channel water away from the foundation of a house or building. Overflowing gutters can cause foundation settling, staircase cracks in brick and block, and a flooded yard. 
         
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.`
-    }, {
-        img: "./img/mainbg.jpeg",
-        name: "Example Service",
-        shortDesc: "A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.",
-        desc: `A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.`
-    }, {
-        img: "./img/mainbg.jpeg",
-        name: "Example Service",
-        shortDesc: "A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.",
-        desc: `A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.`
-    }, {
-        img: "./img/mainbg.jpeg",
-        name: "Example Service",
-        shortDesc: "A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.",
-        desc: `A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.`
-    }, {
-        img: "./img/mainbg.jpeg",
-        name: "Example Service",
-        shortDesc: "A rossssssss s  ssss  ss ss s s ss s s  ss ss s ssss  sss sssssssssssss ssssssssssssssssutine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.",
-        desc: `A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-        
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.
-                
-        A routine wash will extend the life of your commercial property. It keeps the appeal of your business nice and fresh. Includes retail, restaurants, banks, office space, and much more.`
+        Our gutter cleaning service removes all debris and blockage inside the gutter, freeing the water flow.`
     },
+    {
+        img: "./img/mainbg.jpeg",
+        name: "Oxidation removal",
+        shortDesc: "Oxidation is a chemical breakdown of a specific material. Rust is the oxidation of steel. Overtime vinyl, hardie board, and metal oxidize.",
+        desc: `Oxidation is a chemical breakdown of a specific material. Rust is the oxidation of steel. Overtime vinyl, hardie board, and metal oxidize. Oxidation often times occurs to surfaces that have extreme exposure to U.V rays. Oxidation can also occur if certain solutions are misused to clean a house or building. 
+
+        Oxidation leaves a chalky film over the surface. Most of the time you can rub your fingers on the surface, and if a chalky dust is on your fingers, that is oxidation. Oxidized surfaces that are pressure washed, leaves line marks on the surface. 
+        
+        Oxidation removal is necessary before painting. Oxidation will not come off with soft washing, or even pressure washing. It must be removed with solution specifically designed for it, agitation of the surface, proper dwell time, and rinsing. `
+    },
+
 
 ]
 
