39b9aab32a54400c4753338b026c70da352688bd
William Bojczuk <williambojczuk@gmail.com>
2023-08-25 09:57:09 -0400

    added photos

--- src/components/Gallery/galleryData.ts
+++ src/components/Gallery/galleryData.ts
@@ -5,13 +5,37 @@ interface galleryType{
 
 const galleryData: galleryType[] = [
     {
-        src: "./img/gallery/img1.png",
-        title: "Patio Decks"
+        src: "./img/gallery/img1.jpg",
+        title: "Before and After Photo"
     },
     {
-        src: "./img/gallery/img1.png",
-        title: "Patio Decks"
-    }
+        src: "./img/gallery/img3.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img4.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img5.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img6.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img7.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img8.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img2.jpg",
+        title: "Before and After Photo"
+    },
 ]
 
 export default galleryData
--- src/components/Reviews/review_data.ts
+++ src/components/Reviews/review_data.ts
@@ -4,33 +4,44 @@ interface review {
 }
 const reviewData: review[] = [
     {
-        name: "Johnny Appleseed",
+        name: "Sheila Trapp",
         desc: "The house siding needed it's yearly cleaning, so I thought I would add the roof this time to try and remove the algae that had built and was so unsightly. Trey did a wonderful job on both. We even decided to clean the driveway while he was here. Great job on all three surfaces."
     },
     {
-        name: "Johnny Appleseed",
-        desc: "The house siding needed it's yearly cleaning, so I thought I would add the roof this time to try and remove the algae that had built and was so unsightly. Trey did a wonderful job on both. We even decided to clean the driveway while he was here. Great job on all three surfaces."
+        name: "Martha Burton",
+        desc: "Trey did a great job pressure washing our house and pool. He is very professional. He was prompt and very knowledgeable about what was needed to clean different surfaces. We were so thrilled with how the first job turned out that we called him back to wash our new house."
     },
     {
-        name: "Johnny Appleseed",
-        desc: "The house siding needed it's yearly cleaning, so I thought I would add the roof this time to try and remove the algae that had built and was so unsightly. Trey did a wonderful job on both. We even decided to clean the driveway while he was here. Great job on all three surfaces."
+        name: "Cheri Black",
+        desc: `Trey pressure washed my mother’s 25 year old house that had never been washed before.  There was a lot of grime built up.  The house looks amazing!!  The siding looks new again! He also washed the front and back porch.
+        Highly recommend!`
+    },
+    
+    {
+        name: "Richard Pack",
+        desc: "Trey did a great job for us here at our church and I would recommend him to anyone needing anything pressure washed Thumbs up!"
     },
     {
-        name: "Johnny Appleseed",
-        desc: "The house siding needed it's yearly cleaning, so I thought I would add the roof this time to try and remove the algae that had built and was so unsightly. Trey did a wonderful job on both. We even decided to clean the driveway while he was here. Great job on all three surfaces."
+        name: "Dell Black",
+        desc: "I would highly Recommend this business for all of your pressure washing needs.  He just completed my home and I couldn’t be more pleased.   A+ service and very professional!!"
     },
     {
-        name: "Johnny Appleseed",
-        desc: "The house siding needed it's yearly cleaning, so I thought I would add the roof this time to try and remove the algae that had built and was so unsightly. Trey did a wonderful job on both. We even decided to clean the driveway while he was here. Great job on all three surfaces."
+        name: "Michael Taccetta",
+        desc: "I found this service by Facebook advertising me to be interested immediately and then sent a message thru messenger. Trey replied quickly and was available to arrive here for washing off the filthy driveway. He did an amazing job! He showed patience to communicate with me because I’m deaf. I definitely will have him do it again! Thank you, Trey!"
     },
     {
-        name: "Johnny Appleseed",
-        desc: "The house siding needed it's yearly cleaning, so I thought I would add the roof this time to try and remove the algae that had built and was so unsightly. Trey did a wonderful job on both. We even decided to clean the driveway while he was here. Great job on all three surfaces."
+        name: "GA Buttercup",
+        desc: "Superior Pressure Washing did a terrific job pressure washing my house and paver deck. Trey was professional, responsive, throrough, and most importantly did a supurb job. I highly recommend \"Superior Pressure Washing\"!"
     },
     {
-        name: "Johnny Appleseed",
-        desc: "The house siding needed it's yearly cleaning, so I thought I would add the roof this time to try and remove the algae that had built and was so unsightly. Trey did a wonderful job on both. We even decided to clean the driveway while he was here. Great job on all three surfaces."
+        name: "Josh Morrow",
+        desc: "Superior did a great job pressure washing my house and cleaning our gutters.  Very prompt service and great communication."
+    },
+    {
+        name: "Christopher Willingham",
+        desc: "I contacted Trey with Superior Pressure Washing to clean my driveway, back patio and also the vinyl siding on the front & back of my town house. Let me tell you, he brought it from death to life! It’s looks 100x better along with a great price! He is honest & trustworthy and does great work. Also very knowledgeable about the craftsmanship. 100% recommend"
     }
+
 ]
 
 export default reviewData
