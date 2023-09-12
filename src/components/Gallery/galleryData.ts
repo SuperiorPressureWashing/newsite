3adc339b68358097924b6422f312fac04c29a412
William Bojczuk <williambojczuk@gmail.com>
2023-09-12 13:58:31 -0400

    added more beforeafter pics

--- src/components/Gallery/galleryData.ts
+++ src/components/Gallery/galleryData.ts
@@ -5,37 +5,61 @@ interface galleryType{
 
 const galleryData: galleryType[] = [
     {
+        src: "./img/gallery/img0.jpg",
+        title: "Before and After Photo"
+    },
+    {
         src: "./img/gallery/img1.jpg",
         title: "Before and After Photo"
     },
-    // {
-    //     src: "./img/gallery/img2.jpg",
-    //     title: "Before and After Photo"
-    // },
-    // {
-    //     src: "./img/gallery/img4.jpg",
-    //     title: "Before and After Photo"
-    // },
-    // {
-    //     src: "./img/gallery/img5.jpg",
-    //     title: "Before and After Photo"
-    // },
-    // {
-    //     src: "./img/gallery/img6.jpg",
-    //     title: "Before and After Photo"
-    // },
-    // {
-    //     src: "./img/gallery/img7.jpg",
-    //     title: "Before and After Photo"
-    // },
-    // {
-    //     src: "./img/gallery/img8.jpg",
-    //     title: "Before and After Photo"
-    // },
-    // {
-    //     src: "./img/gallery/img3.jpg",
-    //     title: "Before and After Photo"
-    // },
+    {
+        src: "./img/gallery/img2.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img3.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img4.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img5.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img6.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img7.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img8.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img9.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img10.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img11.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img12.jpg",
+        title: "Before and After Photo"
+    },
+    {
+        src: "./img/gallery/img13.jpg",
+        title: "Before and After Photo"
+    }
 ]
 
 export default galleryData
